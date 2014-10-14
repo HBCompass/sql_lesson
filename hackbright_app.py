9d2366eae56bf2cebf89be486f76d5181e89fdab
Hackbright Student <info@hackbrightacademy.com>
2014-10-14 13:01:06 -0700

    Working on expansion section of Part2.

--- hackbright_app.py
+++ hackbright_app.py
@@ -11,6 +11,14 @@ def get_student_by_github(github):
 Student: %s %s
 Github account: %s"""%(row[0], row[1], row[2])
 
+def get_project_by_title(title):
+    query = """ SELECT title, description FROM Projects WHERE title = ?"""
+    DB.execute(query, (title,))
+    row = DB.fetchone()
+    print """\
+Project: %s 
+Description: %s""" %(row[0], row[1])
+
 def connect_to_db():
     global DB, CONN
     CONN = sqlite3.connect("hackbright.db")
@@ -20,8 +28,16 @@ def make_new_student(first_name, last_name, github):
     query = """INSERT into Students values (?, ?, ?)"""
     DB.execute(query, (first_name, last_name, github))
 
-    # CONN.commit()
-    # print "Successfully added student: %s %s"%(first_name, last_name)
+    CONN.commit()
+    print "Successfully added student: %s %s"%(first_name, last_name)
+
+def make_new_project(title, description, max_grade):
+    query = """INSERT into Projects values (?, ?, ?)"""
+    DB.execute(query, (title, description, max_grade))
+
+    CONN.commit()
+    print "Successfully added project: %s"%(title)
+
 
 def main():
     connect_to_db()
@@ -36,6 +52,10 @@ def main():
             get_student_by_github(*args) 
         elif command == "new_student":
             make_new_student(*args)
+        elif command == "title":
+            get_project_by_title(*args)
+        elif command == "new_project":
+            make_new_project(*args)
 
     CONN.close()
 
